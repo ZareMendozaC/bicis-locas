63653d78f3d705723253ea5a06b6cd6ba2a328af
ZareMendozaC <zare.mendozac@gmail.com>
2017-01-12 17:06:52 -0500

    sacando las variables y guardandolas en sus variables

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+function validateForm(){
+	/* Escribe tú código aquí */
+    camposTexto = document.getElementById("formulario").elements;
+    var nombre= camposTexto[0].value;
+    var apellido= camposTexto[1].value;
+    var email= camposTexto[2].value;
+    var contraseña= camposTexto[3].value;
+  }
